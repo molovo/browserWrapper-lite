dd3a7fb4bd755ffca28f475cb80bf102ed353231
Jelmer Borst <me@japborst.net>
2013-05-03 01:17:16 +0200

    Responsiveness
    CSS:
    In order to make it responsive a fixed width is not possible. Using a
    position absolute and left and right works.
    
    JS:
    !!Haven't compiled a minified version!!
    For convenience "/src/" went into "filePath"
    Setting addressBarWidth and addressTxtWidth was redundant as far as I
    could tell - in this way, might needs more testing.
    Set some ratios upon resize.
    
    HTML:
    A little demo page that I used to test it with.

--- src/js/jquery.browserwrapper.js
+++ src/js/jquery.browserwrapper.js
@@ -22,7 +22,7 @@
             browserTitle:   'Your Awesome Title',                 //title to appear on tab
             browserURL:     'http://your.awesome.url.com',  //URL to appear in address bar
             favicon:        'http://browserwrapper.molovo.co.uk/img/favicon.ico',       //URL for favicon to appear on tab
-            filePath:       'browserWrapper-lite/',                          //path to browserWrapper
+            filePath:       'browserWrapper-lite/src/',                          //path to browserWrapper
             shadow:         '0px 5px 15px rgba(0,0,0,0.3)',             //adds a box-shadow to the browser, follows CSS syntax, use '0' to remove
             responsive:     'n'                                         // if 'y', reloads page on window.resize to redraw browser window at a different size
         };
@@ -36,8 +36,8 @@
         var addressBar      = $('.browser-gui .address-bar');
         var height          = act.height();
         var width           = act.width();
-        var addressBarWidth = width - 154;
-        var addressTxtWidth = width - 214;
+        var ratioW          = width / $(window).width() * 100;
+        var ratioH          = height / width * 100;
         var guiHeight       = height + 35;
         var windowHeight    = height + 69;
 
@@ -45,17 +45,19 @@
         var currentContent  = act.html();
 
         //build the controls based on variables above
-        var browserControls = '<div class="browser-gui-controls"><img class="nav-buttons" src="' + defaults.filePath + 'src/img/nav-buttons.png" /><span class="address-bar" style="width: ' + addressBarWidth + 'px;"><img class="address-bar-icon" src="' + defaults.filePath + 'src/img/address-bar-icon.png" /><span class="address-bar-text" style="width: ' + addressTxtWidth + 'px;">' + defaults.browserURL + '</span><img class="address-bar-bookmark" src="' + defaults.filePath + 'src/img/address-bar-bookmark.png" /></span><img class="nav-button-settings" src="' + defaults.filePath + 'src/img/nav-button-settings.png" /></div>';
-        var windowControls  = '<div class="browser-window-controls"><img class="window-button red" src="' + defaults.filePath + 'src/img/red-button.png" /><img class="window-button yellow" src="' + defaults.filePath + 'src/img/yellow-button.png" /><img class="window-button green" src="' + defaults.filePath + 'src/img/green-button.png" /><span class="tab"><span class="tab-wrapper"><img class="favicon" src="' + defaults.favicon + '" /><span class="tabtext">' + defaults.browserTitle + '</span></span></span></div>';
+        var browserControls = '<div class="browser-gui-controls"><img class="nav-buttons" src="' + defaults.filePath + 'img/nav-buttons.png" /><span class="address-bar"><img class="address-bar-icon" src="' + defaults.filePath + 'img/address-bar-icon.png" /><span class="address-bar-text">' + defaults.browserURL + '</span><img class="address-bar-bookmark" src="' + defaults.filePath + 'img/address-bar-bookmark.png" /></span><img class="nav-button-settings" src="' + defaults.filePath + 'img/nav-button-settings.png" /></div>';
+        var windowControls  = '<div class="browser-window-controls"><img class="window-button red" src="' + defaults.filePath + 'img/red-button.png" /><img class="window-button yellow" src="' + defaults.filePath + 'img/yellow-button.png" /><img class="window-button green" src="' + defaults.filePath + 'img/green-button.png" /><span class="tab"><span class="tab-wrapper"><img class="favicon" src="' + defaults.favicon + '" /><span class="tabtext">' + defaults.browserTitle + '</span></span></span></div>';
 
         //expand div and repopulate with content and browser styling
-        act.height(windowHeight).html('<div class="browser-window-border" style="height: ' + windowHeight + 'px; box-shadow: ' + defaults.shadow  + ';">' + windowControls + '<div class="browser-gui" style="height: ' + guiHeight + 'px;">' + browserControls + currentContent + '</div></div>').trigger('refresh');
+        act.html('<div class="browser-window-border" style="box-shadow: ' + defaults.shadow  + ';">' + windowControls + '<div class="browser-gui">' + browserControls + currentContent + '</div></div>').trigger('refresh');
 
 
         if ( defaults.responsive == 'y' ) {
             //refresh page to force browser redraw on resize
             $(window).resize(function(){
-                location.reload();
+                act.width( ratioW + '%');
+                act.height(0).css('padding-bottom', ratioH + '%');
+                $('.browser-content').width('100%');
             });
         }
 
